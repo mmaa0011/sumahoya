3f967f45d32dfd8054d7efb6f6be99e4a7e8c6c4
mmaa0011 <masaki_23n1101222@nnn.ed.jp>
2025-11-06 15:20:37 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-let remaining = 10;
+let remaining = 30;
 const elCountdown = document.getElementById("countdown");
 const elNotify = document.getElementById("notify");
 
 const timer = setInterval(() => {
   remaining--;
   const m = String(Math.floor(remaining / 60)).padStart(2, "0");
-  const s = String(remaining % 10).padStart(2, "0");
+  const s = String(remaining % 60).padStart(2, "0");
   elCountdown.textContent = `${m}:${s}`;
 
   if (remaining <= 0) {
